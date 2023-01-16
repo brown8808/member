2a1fdc45e706fbe231a63cc058697aaedb4ee5b3
brown8808 <phh8808@gmail.com>
2023-01-16 20:21:12 +0900

    first

---
+++ src/main/java/com/brown8808/member/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.brown8808.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/test/java/com/brown8808/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brown8808.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
